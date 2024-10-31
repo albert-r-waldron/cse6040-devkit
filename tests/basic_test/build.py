00b7c19368c6fc2cc358c2259427c30348f6e83d
Your Name <you@example.com>
2024-10-30 20:37:10 -0400

    Empty build

---
+++ tests/basic_test/build.py
@@ -0,0 +1,8 @@
+from cse6040_devkit.assignment import AssignmentBuilder
+import logging
+
+logger = logging.getLogger(__name__)
+builder = AssignmentBuilder()
+
+if __name__ == '__main__':
+    builder.build()
